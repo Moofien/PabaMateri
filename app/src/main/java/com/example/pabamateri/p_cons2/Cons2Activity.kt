3cc612998cd08c08f05e8bc76eb528c87f902421
Moofien <witherclark@gmail.com>
2025-12-26 22:39:12 +0700

    Constrain UI 2

---
+++ app/src/main/java/com/example/pabamateri/p_cons2/Cons2Activity.kt
@@ -0,0 +1,15 @@
+package com.example.pabamateri.p_cons2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.pabamateri.R
+
+class Cons2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cons2)
+    }
+}
