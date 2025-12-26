1ae99589f74d71aa6d807f144dd4d3e704b4180f
Moofien <witherclark@gmail.com>
2025-12-26 22:35:21 +0700

    Layout

--- app/src/main/java/com/example/pabamateri/MainActivity.kt
+++ app/src/main/java/com/example/pabamateri/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.pabamateri
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.pabamateri.p_layout.LayoutActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnP1 = findViewById<Button>(R.id.btn_layout)
+        val btnP2 = findViewById<Button>(R.id.btn_cons1)
+        val btnP3 = findViewById<Button>(R.id.btn_cons2)
+        val btnP4 = findViewById<Button>(R.id.btn_exp)
+
+        btnP1.setOnClickListener {
+            val intent = Intent(this, LayoutActivity::class.java)
+            startActivity(intent)
+        }
+        btnP2.setOnClickListener {
+            val intent = Intent(this, Cons1Activity::class.java)
+            startActivity(intent)
+        }
+        btnP3.setOnClickListener {
+            val intent = Intent(this, Cons2Activity::class.java)
+            startActivity(intent)
+        }
+        btnP4.setOnClickListener {
+            val intent = Intent(this, ExplisitActivity::class.java)
+            startActivity(intent)
         }
     }
 }
